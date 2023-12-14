1163b20e73fd1db272e181c3c60c886eaed64b00
Leovany <348083098@qq.com>
2023-12-14 23:13:39 +0800

    Knife4j增加接口屏蔽开关

--- src/main/java/com/leovany/usercenter/config/Knife4jConfig.java
+++ src/main/java/com/leovany/usercenter/config/Knife4jConfig.java
@@ -1,9 +1,7 @@
 package com.leovany.usercenter.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,15 +9,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 自定义 Knife4j 接口文档的配置
  */
 @Configuration
-//@EnableSwagger2WebMvc
-//@Profile({"dev", "test"})
-//@EnableKnife4j
+@EnableSwagger2
 public class Knife4jConfig {
 
     @Bean(value = "defaultApi2")
